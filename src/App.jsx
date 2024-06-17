2243c9e1d49ebb85b6c56a08ebaaf533486649e2
dlouie2000 <70512125+dlouie2000@users.noreply.github.com>
2024-06-17 20:31:18 +0800

    1st
    1st

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+      <div className="flixed top-0 -z-10 h-full"></div>
+      {/* <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>    */}
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
+    <div className="container mx-auto px-8">
+      <Navbar />
+      <Hero />
+      
+
+      </div>
+    </div>
+  );
+};
+
+export default App;
